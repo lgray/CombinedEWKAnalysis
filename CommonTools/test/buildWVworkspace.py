c54b50083c2f26a4cf32e55ed5fd47d08739243f
Lindsey Gray <lindsey.gray@gmail.com>
2013-07-03 04:16:30 -0500

    add and combine electron channel

--- CommonTools/test/buildWVworkspace.py
+++ CommonTools/test/buildWVworkspace.py
@@ -1,22 +1,33 @@
 import pyroot_logon
 import limits
 import os
+import sys
 
 from ROOT import *
 
+lType = sys.argv[1] 
+codename = ""
+
+if( lType == "muon" ) :
+    codename = "mu"
+elif( lType == "electron" ):
+    codename = "el"
+else:
+    raise RuntimeError('InvalidLepton','You may only choose between "muon" and "electron" channels.')
+
 basepath = '%s/src/CombinedEWKAnalysis/CommonTools/data/WV_semileptonic'%os.environ['CMSSW_BASE']
 
-f = TFile('%s/mu_boosted.root'%basepath)
+f = TFile('%s/%s_boosted.root'%(basepath,codename))
 
 background = f.Get('background')
-background_muboosted_backshapeUp = f.Get('background_muboosted_backshapeUp')
-background_muboosted_backshapeDown = f.Get('background_muboosted_backshapeDown')
+background_backshapeUp = f.Get('background_%sboosted_backshapeUp'%codename)
+background_backshapeDown = f.Get('background_%sboosted_backshapeDown'%codename)
 data_obs = f.Get('data_obs')
 diboson = f.Get('diboson')
 
 background.Add(diboson, -1.)
 
-theWS = RooWorkspace('WV_muboosted', 'WV_muboosted')
+theWS = RooWorkspace('WV_%sboosted'%codename, 'WV_%sboosted'%codename)
 
 wpt = theWS.factory('W_pt[%f,%f]' % (data_obs.GetBinLowEdge(1), 
                                      data_obs.GetBinLowEdge(data_obs.GetNbinsX())+data_obs.GetBinWidth(data_obs.GetNbinsX())))
@@ -32,33 +43,35 @@ dg1 = theWS.factory('dg1[0.]')
 vars = RooArgList(wpt)
 varSet = RooArgSet(wpt)
 
-data = RooDataHist('data_obs', 'data_obs_WV_mu', vars, data_obs)
-bkgHist = RooDataHist('WV_semileptonic_bkg_mu',
-                      'WV_semileptonic_bkg_mu',
+data = RooDataHist('data_obs', 'data_obs_WV_%s'%codename, vars, data_obs)
+bkgHist = RooDataHist('WV_semileptonic_bkg_%s'%codename,
+                      'WV_semileptonic_bkg_%s'%codename,
                       vars,
                       background)
-bkgHist_systUp = RooDataHist('WV_semileptonic_bkg_mu_muboosted_backshapeUp',
-                             'WV_semileptonic_bkg_mu_muboosted_backshapeUp',
+bkgHist_systUp = RooDataHist('WV_semileptonic_bkg_%s_%sboosted_backshapeUp'%(codename,codename),
+                             'WV_semileptonic_bkg_%s_%sboosted_backshapeUp'%(codename,codename),
                              vars,
-                             background_muboosted_backshapeUp)
-bkgHist_systDown = RooDataHist('WV_semileptonic_bkg_mu_muboosted_backshapeDown',
-                               'WV_semileptonic_bkg_mu_muboosted_backshapeDown',
+                             background_backshapeUp)
+bkgHist_systDown = RooDataHist('WV_semileptonic_bkg_%s_%sboosted_backshapeDown'%(codename,
+                                                                                 codename),
+                               'WV_semileptonic_bkg_%s_%sboosted_backshapeDown'%(codename,
+                                                                                 codename),
                                vars,
-                               background)
+                               background_backshapeDown)
 
-dibosonHist = RooDataHist('WV_semileptonic_SM_mu_rawshape',
-                          'WV_semileptonic_SM_mu_rawshape',
+dibosonHist = RooDataHist('WV_semileptonic_SM_%s_rawshape'%codename,
+                          'WV_semileptonic_SM_%s_rawshape'%codename,
                           vars,
                           diboson)
-dibosonPdf = RooHistFunc('WV_semileptonic_SM_mu_shape',
-                        'WV_semileptonic_SM_mu_shape',
-                        varSet,
-                        dibosonHist)
+dibosonPdf = RooHistFunc('WV_semileptonic_SM_%s_shape'%codename,
+                         'WV_semileptonic_SM_%s_shape'%codename,
+                         varSet,
+                         dibosonHist)
 
 
 
-aTGC = RooATGCFunction('ATGC_shapescale_WWgammaZ_WV_atgc_semileptonic_mu',
-                       'ATGC_shapescale_mu',
+aTGC = RooATGCFunction('ATGC_shapescale_WWgammaZ_WV_atgc_semileptonic_%s'%codename,
+                       'ATGC_shapescale_%s'%codename,
                        wpt,
                        lz,
                        dkg,
@@ -67,8 +80,8 @@ aTGC = RooATGCFunction('ATGC_shapescale_WWgammaZ_WV_atgc_semileptonic_mu',
 
 
 
-aTGCPdf = RooATGCSemiAnalyticPdf('ATGCPdf_WWgammaZ_WV_atgc_semileptonic_mu',
-                                 'ATGCPdf_WV_mu',
+aTGCPdf = RooATGCSemiAnalyticPdf('ATGCPdf_WWgammaZ_WV_atgc_semileptonic_%s'%codename,
+                                 'ATGCPdf_WV_%s'%codename,
                                  wpt,
                                  dkg,
                                  lz,                                 
@@ -84,6 +97,6 @@ getattr(theWS, 'import')(aTGCPdf)
 
 theWS.Print()
 
-fout = TFile('mu_boosted_ws.root', 'recreate')
+fout = TFile('%s_boosted_ws.root'%codename, 'recreate')
 theWS.Write()
 fout.Close()
