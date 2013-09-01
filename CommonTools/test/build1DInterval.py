294a864239e899a7349149502b952630b1bdd074
Lindsey Gray <lindsey.gray@gmail.com>
2013-09-01 10:29:31 -0500

    make output easier to understand, add 95% CL

--- CommonTools/test/build1DInterval.py
+++ CommonTools/test/build1DInterval.py
@@ -21,26 +21,42 @@ minVal = histo.GetBinContent(minBin)
 print 'Found profile-likelihood mimimum value %.3f at %.3f'%(minVal,
                                                              minBinCenter)
 #skip best fit value which is entry 0
-bounds = []
-lastBelowErr = False
-lastAboveErr = True
+bounds68 = []
+bounds95 = []
+lastBelowErr68 = False
+lastAboveErr68 = True
+lastBelowErr95 = False
+lastAboveErr95 = True
 for i in xrange(nEntries):
     limit.GetEntry(i+1)
     NLL = limit.GetLeaf("deltaNLL").GetValue()
     if NLL - minVal <= 0.5:
-        if lastAboveErr:
-            bounds.append([])
-            bounds[-1].append(limit.GetLeaf(parm).GetValue())
-        lastBelowErr = True
-        lastAboveErr = False
+        if lastAboveErr68:
+            bounds68.append([])
+            bounds68[-1].append(limit.GetLeaf(parm).GetValue())
+        lastBelowErr68 = True
+        lastAboveErr68 = False
     else:
-        if lastBelowErr:
-            bounds[-1].append(limit.GetLeaf(parm).GetValue())
-        lastBelowErr = False
-        lastAboveErr = True
+        if lastBelowErr68:
+            bounds68[-1].append(limit.GetLeaf(parm).GetValue())
+        lastBelowErr68 = False
+        lastAboveErr68 = True
+    if NLL - minVal <= 1.92:
+        if lastAboveErr95:
+            bounds95.append([])
+            bounds95[-1].append(limit.GetLeaf(parm).GetValue())
+        lastBelowErr95 = True
+        lastAboveErr95 = False
+    else:
+        if lastBelowErr95:
+            bounds95[-1].append(limit.GetLeaf(parm).GetValue())
+        lastBelowErr95 = False
+        lastAboveErr95 = True
 file.Close()
 
-boundstext = ['[%.3g,%.3g]'%(b[0],b[1]) for b in bounds]
-print 'U'.join(boundstext)
+boundstext68 = ['[%.3g,%.3g]'%(b[0],b[1]) for b in bounds68]
+boundstext95 = ['[%.3g,%.3g]'%(b[0],b[1]) for b in bounds95]
+print '68% CL Limit:','U'.join(boundstext68)
+print '95% CL Limit:','U'.join(boundstext95)
 
 
